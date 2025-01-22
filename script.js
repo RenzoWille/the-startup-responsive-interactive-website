19582a443261920e2edb3bac2e034e68f9e444d0
RenzoWille <renzo.wille@hva.nl>
2025-01-22 17:02:21 +0100

    #10

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// zorg ervoor dat js de zoekbalk kan vinden 
-
-let mijnZoekbalk = document.querySelector('#search-input');
-let vacatureBlok = document.querySelectorAll('.vacatureblok');
-
-// voeg een event toe 
-
-mijnZoekbalk.addEventListener('input', searchTable);
-
-// voeg een functie toe
-
-function searchTable() {
-    vacatureBlok.forEach((row) => {
-        let vacature_data = row.textContent.toLocaleLowerCase(),
-            search_data = mijnZoekbalk.value.toLocaleLowerCase();
-
-        row.classList.toggle('hide', vacature_data.indexOf(search_data) < 0);
-    });
-}
-
 //zorg dat de dom de selector kan vinden 
 
 let mijnForm = document.querySelector('form');
